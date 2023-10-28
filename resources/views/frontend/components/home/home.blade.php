cf16bfd755a4510fe76f8b73dbab104c4eb10c00
zidankhan86 <zidankhan718@gmail.com>
2023-10-28 21:55:02 +0600

    landing

--- resources/views/frontend/components/home/home.blade.php
+++ resources/views/frontend/components/home/home.blade.php
@@ -1,1 +1,17 @@
-<h1 style="color: red">That Will Be Landing Page........</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Car Tracking </title>
+</head>
+<body>
+    <header style="background-color: #3498db; color: #fff; text-align: center; padding: 20px;">
+        <h1 style="font-size: 36px; margin: 0;">Car Tracking Landing Will Be Add Soon...</h1><br><br>
+        <nav style="font-size: 18px;">
+            <a style="color: #fff; text-decoration: none; margin: 0 10px;" href="{{ route('home') }}">Home</a>
+            <a style="color: #fff; text-decoration: none; margin: 0 10px;" href="{{ route('login') }}">Login</a>
+            <a style="color: #fff; text-decoration: none; margin: 0 10px;" href="{{ route('registration') }}">Registration</a>
+            <a style="color: #fff; text-decoration: none; margin: 0 10px;" href="{{ route('logout') }}">Logout</a>
+        </nav>
+    </header>
+</body>
+</html>
--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -1,7 +1,5 @@
-@extends('frontend\layout\app')
 
-@section('content')
 
 @include('frontend\components\home\home')
 
-@endsection
+
