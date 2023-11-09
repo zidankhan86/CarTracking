26cc7bd1df8df288d9eafa3451fb3357492c7493
zidankhan86 <zidankhan718@gmail.com>
2023-11-09 22:56:52 +0600

    Profile

--- resources/views/backend/components/profile/profile.blade.php
+++ resources/views/backend/components/profile/profile.blade.php
@@ -36,7 +36,7 @@
                                             d="M9 18h-3a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-3l-3 3l-3 -3z">
                                         </path>
                                     </svg> Email: <strong>{{ auth()->user()->email }}</strong>
-                                </div>
+                                </div><br>
 
                                 <div>
                                     <!-- Download SVG icon from http://tabler-icons.io/i/clock -->
@@ -47,7 +47,55 @@
                                         <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0 M12 7v5l3 3" />
                                     </svg>Created at: <strong>{{auth()->user()->created_at->format('h:i A, d M Y') }}
                                     </strong>
+                                </div><br>
+
+
+                                <div>
+                                    <!-- Download SVG icon from http://tabler-icons.io/i/clock -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ambulance" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                                        <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                                        <path d="M5 17h-2v-11a1 1 0 0 1 1 -1h9v12m-4 0h6m4 0h2v-6h-8m0 -5h5l3 5"></path>
+                                        <path d="M6 10h4m-2 -2v4"></path>
+                                     </svg>Car Number: <strong>{{auth()->user()->car_number }}
+                                    </strong>
+                                </div><br>
+
+                                <div>
+                                    <!-- Download SVG icon from http://tabler-icons.io/i/clock -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-layout-bottombar-expand-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M18 3a3 3 0 0 1 2.995 2.824l.005 .176v12a3 3 0 0 1 -2.824 2.995l-.176 .005h-12a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-12a3 3 0 0 1 2.824 -2.995l.176 -.005h12zm0 2h-12a1 1 0 0 0 -.993 .883l-.007 .117v9h14v-9a1 1 0 0 0 -.883 -.993l-.117 -.007zm-5.387 3.21l.094 .083l2 2a1 1 0 0 1 -1.32 1.497l-.094 -.083l-1.293 -1.292l-1.293 1.292a1 1 0 0 1 -1.32 .083l-.094 -.083a1 1 0 0 1 -.083 -1.32l.083 -.094l2 -2a1 1 0 0 1 1.32 -.083z" stroke-width="0" fill="currentColor"></path>
+                                     </svg>Experience : <strong>{{auth()->user()->experience }}
+                                    </strong>
+                                </div><br>
+
+
+                                <div>
+                                    <!-- Download SVG icon from http://tabler-icons.io/i/clock -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-credit-card-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M22 10v6a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-6h20zm-14.99 4h-.01a1 1 0 1 0 .01 2a1 1 0 0 0 0 -2zm5.99 0h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0 -2zm5 -10a4 4 0 0 1 4 4h-20a4 4 0 0 1 4 -4h12z" stroke-width="0" fill="currentColor"></path>
+                                     </svg>Voter Number: <strong>{{auth()->user()->voter_no }}
+                                    </strong>
+                                </div><br>
+
+
+                                <div>
+                                    <!-- Download SVG icon from http://tabler-icons.io/i/clock -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-certificate-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 15m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                                        <path d="M10 7h4"></path>
+                                        <path d="M10 18v4l2 -1l2 1v-4"></path>
+                                        <path d="M10 19h-2a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-2"></path>
+                                     </svg>License: <strong>{{auth()->user()->license }}
+                                    </strong>
                                 </div>
+
+
+
                             </div>
                         </div>
                     </div>
